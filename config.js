64ce6c79aff2e857b7750dd7d4ca2421b124755c
s776t7jqgg-sketch <s776t7jqgg@privaterelay.appleid.com>
2026-01-30 19:20:20 +0000

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
+            text: "Do you love me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
             secretAnswer: "I like you more than i could put into words ! ❤️"           // Secret hover message
